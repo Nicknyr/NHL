bf20e776f5f0b5d06adcafb2ece36787373c447e
Nick <nick_kinlen@hotmail.com>
2018-11-18 17:44:28 -0500

    Displaying all Rangers stats from fetchRangerStats i.e. https://statsapi.web.nhl.com/api/v1/teams/3/stats

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import {connect} from 'react-redux';
 import { fetchRangersStats } from './actions/fetchRangersStats-action';
@@ -17,12 +16,66 @@ class App extends Component {
 
     // Accessing this.props.rangersStat.data and it's nested properties directly in the reducer
     console.log(this.props.rangersStats.data);
-    console.log('Losses : ' + this.props.rangersStats.losses);
+    const losses = this.props.rangersStats.losses;
+    const evGGARatio = this.props.rangersStats.evGGARatio;
+    const faceOffWinPercentage = this.props.rangersStats.faceOffWinPercentage;
+    const faceOffsLost = this.props.rangersStats.faceOffsLost;
+    const faceOffsTaken = this.props.rangersStats.faceOffsTaken;
+    const faceOffsWon = this.props.rangersStats.faceOffsWon;
+    const gamesPlayed = this.props.rangersStats.gamesPlayed;
+    const goalsAgainstPerGame = this.props.rangersStats.goalsAgainstPerGame;
+    const goalsPerGame = this.props.rangersStats.goalsPerGame;
+    const ot = this.props.rangersStats.ot;
+    const penaltyKillPercentage = this.props.rangersStats.penaltyKillPercentage;
+    const powerPlayGoals = this.props.rangersStats.powerPlayGoals;
+    const powerPlayGoalsAgainst = this.props.rangersStats.powerPlayGoalsAgainst;
+    const powerPlayOpportunities = this.props.rangersStats.powerPlayOpportunities;
+    const powerPlayPercentage = this.props.rangersStats.powerPlayPercentage;
+    const ptPctg = this.props.rangersStats.ptPctg;
+    const pts = this.props.rangersStats.pts;
+    const savePctg = this.props.rangersStats.savePctg;
+    const shootingPctg = this.props.rangersStats.shootingPctg;
+    const shotsAllowed = this.props.rangersStats.shotsAllowed;
+    const shotsPerGame = this.props.rangersStats.shotsPerGame;
+    const winLeadFirstPer = this.props.rangersStats.winLeadFirstPer;
+    const winLeadSecondPer = this.props.rangersStats.winLeadSecondPer;
+    const winOppScoreFirst = this.props.rangersStats.winOppScoreFirst;
+    const winOutshootOpp = this.props.rangersStats.winOutshootOpp;
+    const winOutshotByOpp = this.props.rangersStats.winOutshotByOpp;
+    const winScoreFirst = this.props.rangersStats.winScoreFirst;
+    const wins = this.props.rangersStats.wins;
 
 
     return (
       <div className="App">
-        {/*stats*/}
+        <div>Losses : {losses}</div>
+        <div>evGGARatio : {evGGARatio}</div>
+        <div>faceOffWinPercentage : {faceOffWinPercentage}</div>
+        <div>faceOffsLost : {faceOffsLost}</div>
+        <div>faceOffsTaken : {faceOffsTaken}</div>
+        <div>faceOffsWon : {faceOffsWon}</div>
+        <div>gamesPlayed : {gamesPlayed}</div>
+        <div>goalsAgainstPerGame : {goalsAgainstPerGame}</div>
+        <div>goalsPerGame : {goalsPerGame}</div>
+        <div>ot : {ot}</div>
+        <div>penaltyKillPercentage : {penaltyKillPercentage}</div>
+        <div>powerPlayGoals : {powerPlayGoals}</div>
+        <div>powerPlayGoalsAgainst : {powerPlayGoalsAgainst}</div>
+        <div>powerPlayOpportunities : {powerPlayOpportunities}</div>
+        <div>powerPlayPercentage : {powerPlayPercentage}</div>
+        <div>ptPctg : {ptPctg}</div>
+        <div>pts : {pts}</div>
+        <div>savePctg : {savePctg}</div>
+        <div>shootingPctg : {shootingPctg}</div>
+        <div>shotsAllowed : {shotsAllowed}</div>
+        <div>shotsPerGame : {shotsPerGame}</div>
+        <div>winLeadFirstPer : {winLeadFirstPer}</div>
+        <div>winLeadSecondPer : {winLeadSecondPer}</div>
+        <div>winOppScoreFirst : {winOppScoreFirst}</div>
+        <div>winOutshootOpp : {winOutshootOpp}</div>
+        <div>winOutshotByOpp : {winOutshotByOpp}</div>
+        <div>winScoreFirst : {winScoreFirst}</div>
+        <div>wins : {wins}</div>
       </div>
     );
   }
--- src/reducers/fetchRangersStats-reducer.js
+++ src/reducers/fetchRangersStats-reducer.js
@@ -55,7 +55,33 @@ export default function reducer (state = {
                 data: action.payload.stat,
                 // Accessing nested props from API directly here rather than in Object.entires in App.js
                 gamesPlayed: action.payload.stat.gamesPlayed,
-                losses: action.payload.stat.losses
+                losses: action.payload.stat.losses,
+                evGGARatio: action.payload.stat.evGGARatio,
+                faceOffWinPercentage: action.payload.stat.faceOffWinPercentage,
+                faceOffsLost: action.payload.stat.faceOffsLost,
+                faceOffsTaken: action.payload.stat.faceOffsTaken,
+                faceOffsWon: action.payload.stat.faceOffsWon,
+                goalsAgainstPerGame: action.payload.stat.goalsAgainstPerGame,
+                goalsPerGame: action.payload.stat.goalsPerGame,
+                ot: action.payload.stat.ot,
+                penaltyKillPercentage: action.payload.stat.penaltyKillPercentage,
+                powerPlayGoals: action.payload.stat.powerPlayGoals,
+                powerPlayGoalsAgainst: action.payload.stat.powerPlayGoalsAgainst,
+                powerPlayOpportunities: action.payload.stat.powerPlayOpportunities,
+                powerPlayPercentage: action.payload.stat.powerPlayPercentage,
+                ptPctg: action.payload.stat.ptPctg,
+                pts: action.payload.stat.pts,
+                savePctg: action.payload.stat.savePctg,
+                shootingPctg: action.payload.stat.shootingPctg,
+                shotsAllowed: action.payload.stat.shotsAllowed,
+                shotsPerGame: action.payload.stat.shotsPerGame,
+                winLeadFirstPer: action.payload.stat.winLeadFirstPer,
+                winLeadSecondPer: action.payload.stat.winLeadSecondPer,
+                winOppScoreFirst: action.payload.stat.winOppScoreFirst,
+                winOutshootOpp: action.payload.stat.winOutshootOpp,
+                winOutshotByOpp: action.payload.stat.winOutshotByOpp,
+                winScoreFirst: action.payload.stat.winScoreFirst,
+                wins: action.payload.stat.wins
             }
         }
         default: return state;
